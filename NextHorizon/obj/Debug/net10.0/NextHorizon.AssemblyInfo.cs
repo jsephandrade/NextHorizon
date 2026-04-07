1ffeed67a4ac107017bfccdb112c92890f4cd4cb
Harvy Gonzales <gonzales.kerbs187@gmail.com>
2026-04-07 16:35:52 +0800

    Latest Ordermanagement update UI and backend

--- NextHorizon/obj/Debug/net10.0/NextHorizon.AssemblyInfo.cs
+++ NextHorizon/obj/Debug/net10.0/NextHorizon.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NextHorizon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61f55a44e44f656fd23afb50c0cdc7adc36f5da2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee67c5e01bd0babe1eb7afddb1fbaff865aa685c")]
 [assembly: System.Reflection.AssemblyProductAttribute("NextHorizon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NextHorizon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
